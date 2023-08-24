52d85119a2457ea63c9ea2be2f758f5341ef71c7
texhnolyze <joegriepen@gmail.com>
2023-08-24 14:23:06 +0200

    feat(userId): make it an url param
    to be able to associate the received accessToken with a user

--- index.js
+++ index.js
@@ -10,11 +10,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 let sdk;
 
-app.post("/accept-user-token", async (req, res) => {
+app.post("/:userId/auth", async (req, res) => {
   let data = req.body;
   sdk = SpotifyApi.withAccessToken("client-id", data);
-  const playbackState = await sdk.player.getPlaybackState();
-  res.json(playbackState || {});
+  const playbackState = (await sdk.player.getPlaybackState()) || {};
+  console.log(
+    `Successfully requested playback state for user ${req.params.userId}`
+  );
+  playbackState.userId = req.params.userId;
+  res.json(playbackState);
 });
 
 app.listen(3001, () => {
