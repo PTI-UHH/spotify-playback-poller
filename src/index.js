3e64dd9e792da97f6a5079201a8c7fec5dc6f91b
texhnolyze <joegriepen@gmail.com>
2023-10-17 20:24:56 +0200

    fix(polling): parallel collection of playback data
    of all active users' data with `Promise.all()`, while also not saving
    empty data responses if a user is currently not using spotify

--- src/index.js
+++ src/index.js
@@ -62,162 +62,40 @@ app.post("/user/:userId/auth", async (req, res) => {
   }
 });
 
-// app.put('/:userId/update', async (req, res) => {
-//   const user_id = req.params.userId
-//   const post = await prisma.post.update({
-//     where: { user_id },
-//     data: { access_token: req.body.access_token},
-//   })
-//   res.json(post)
-// })
-
-// app.post("/:userId/auth", async (req, res) => {
-//   console.log("Start1")
-//   let data = req.body;
-
-//   let sql = `SELECT *
-//            FROM users
-//            WHERE user_id  = ?`;
-
-//   const userExists = await new Promise((resolve, reject) =>
-//       db.get(sql, [req.params.userId], (err, row) => {
-//     if (err) {
-//       reject(err.message);
-//     }
-//     resolve(row);
-//   }));
-
-//   if(!userExists){
-//    await new Promise((resolve, reject) => db.run("INSERT INTO users (user_id, access_token, refresh_token) VALUES (?, ?, ?)", [req.params.userId, data.access_token, data.refresh_token], (err) => {
-//      if(err) {
-//        reject(err.message);
-//      }
-//      resolve();
-//    }))
-//   }else{
-//     await new Promise((resolve, reject) => db.run("UPDATE users SET access_token = ?, refresh_token = ? WHERE user_id = ?", [data.access_token, data.refresh_token, req.params.userId], (err) => {
-//       if(err) {
-//         reject(err.message);
-//       }
-//       resolve();
-//     }))
-
-//   }
-//   console.log("Ok");
-
-//   res.json({message: "ok"});
-// });
-
-/*app.post("/:userId/sample", async (req, res) =>{
-  const user_id = req.params.userId
-  const userData = await prisma.user.findUnique({
-    where: {
-      user_id: user_id
-    },
-  })
-  //console.log(userData);
-
-  res(userData);
-});*/
-
-// app.post("/:userId/sample", async (req, res) =>{
-
-//   let sql = `SELECT *
-//            FROM users
-//            WHERE user_id  = ?`;
-
-//   const {access_token, refresh_token} = await new Promise((resolve, reject) =>
-//       db.get(sql, [req.params.userId], (err, row) => {
-//         if (err) {
-//           reject(err.message);
-//         }
-//         resolve(row);
-//       }));
-
-//   sdk = SpotifyApi.withAccessToken(process.env.SPOTIFY_CLIENT_ID, {access_token, refresh_token});//{access_token: data.access_token});
-
-//   const playbackState = (await sdk.player.getPlaybackState()) || {};
-//   const insertsql = "INSERT INTO playback_data (user_id, data) VALUES (?, ?)";
-//   await new Promise((resolve, reject) => db.run(insertsql, [req.params.userId, JSON.stringify(playbackState)], (err) => {
-//     if(err) {
-//       reject(err.message);
-//     }
-//     resolve();
-//   }))
-
-//   console.log(
-//       `Successfully requested playback state for user ${req.params.userId}`
-//   );
-//   playbackState.userId = req.params.userId;
-//   res.json(playbackState);
-//   //res.json({access_token, refresh_token});
-// });
-///////////////////////////
-async function sampleUsers() {
-  const activeUsers = await prisma.user.findMany({
-    where: { active: true },
+async function saveUserPlaybackState(user) {
+  const { access_token, refresh_token, id } = user;
+  const sdk = SpotifyApi.withAccessToken(process.env.SPOTIFY_CLIENT_ID, {
+    access_token,
+    refresh_token,
   });
-  console.log(activeUsers);
 
-  activeUsers.map(async (user) => {
-    const { access_token, refresh_token, id } = user;
-    const sdk = SpotifyApi.withAccessToken(process.env.SPOTIFY_CLIENT_ID, {
-      access_token,
-      refresh_token,
-    });
-    let playbackState = {};
-    try {
-      playbackState = (await sdk.player.getPlaybackState()) || {};
-    } catch (e) {
-      console.log(e.message);
-    }
+  let playbackState = {};
 
+  try {
+    playbackState = await sdk.player.getPlaybackState();
+  } catch (e) {
+    console.error(e.message);
+  }
+
+  if (playbackState) {
     await prisma.playbackData.create({
       data: {
         userId: id,
         data: JSON.stringify(playbackState),
       },
     });
+  }
+}
+
+async function savePlaybackDataForAllActiveUsers() {
+  const activeUsers = await prisma.user.findMany({
+    where: { active: true },
   });
 
-  //     let playbackState = {}
-  //const {access_token, refresh_token, user_id} = active;
+  await Promise.all(activeUsers.map(saveUserPlaybackState));
 }
-//   const usersql = "SELECT * FROM users"
-//   const rows = await new Promise((resolve, reject) => db.all(usersql, [], (err, rows) => {
-//     if(err) {
-//       reject(err.message);
-//     }
-//     resolve(rows);
-//   }))
 
-//   await rows.map(async (row) => {
-//     const {access_token, refresh_token, user_id} = row;
-//     console.log({access_token, refresh_token});
-//     const result = await fetch("https://api.spotify.com/v1/me/player", {
-//       method: "GET", headers: { Authorization: `Bearer ${access_token}` }
-//     });
-//     console.log("ERGEBNIS", result);
-//     const sdk = SpotifyApi.withAccessToken(process.env.SPOTIFY_CLIENT_ID, {access_token, refresh_token});
-//     let playbackState = {}
-//     try{
-//      playbackState = (await sdk.player.getPlaybackState()) || {};
-//     }catch (e){
-//       console.log(e.message);
-//     }
-//     const insertsql = "INSERT INTO playback_data (user_id, data) VALUES (?, ?)";
-//     await new Promise((resolve, reject) => db.run(insertsql, [user_id, JSON.stringify(playbackState)], (err) => {
-//       if(err) {
-//         reject(err.message);
-//       }
-//       resolve();
-//     }))
-
-//   })
-
-// }
-
-setInterval(sampleUsers, 10000);
+setInterval(savePlaybackDataForAllActiveUsers, 10000);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
