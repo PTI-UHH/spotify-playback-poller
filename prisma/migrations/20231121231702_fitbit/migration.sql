bac178fd1201f176f60f0d24cac38a123e9731cb
texhnolyze <joegriepen@gmail.com>
2023-11-22 00:19:27 +0100

    feat(FitbitData): add prisma FitbitData schema

---
+++ prisma/migrations/20231121231702_fitbit/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `FitbitData` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` VARCHAR(191) NOT NULL,
+    `data` TEXT NOT NULL,
+    `timestamp` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `FitbitData` ADD CONSTRAINT `FitbitData_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
